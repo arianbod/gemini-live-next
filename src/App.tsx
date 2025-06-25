240a7b28911c960aac87fb3d7d9fb0018e9ccfde
Arian <130873720+arianbod@users.noreply.github.com>
2025-06-25 11:52:36 +0300

    Revert "v.0.1"
    This reverts commit 5dce22c8d836c8c08d7bbc66b767200e0eee5a3b.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-// components/App.tsx
-'use client';
+// src/App.tsx
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import { useRef, useState, useEffect } from 'react';
 import './App.scss';
-import { LiveAPIProvider } from '../contexts/LiveAPIContext';
-import SidePanel from './side-panel/SidePanel';
-import { Altair } from './altair/Altair';
-import ControlTray from './control-tray/ControlTray';
+import { LiveAPIProvider } from './contexts/LiveAPIContext';
+import SidePanel from './components/side-panel/SidePanel';
+import { Altair } from './components/altair/Altair';
+import ControlTray from './components/control-tray/ControlTray';
 import cn from 'classnames';
-import { LiveClientOptions } from '../types';
+import { LiveClientOptions } from './types';
 
-// Get API key from environment variables (Next.js format)
-const API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY as string;
+const API_KEY = process.env.REACT_APP_GEMINI_API_KEY as string;
 if (typeof API_KEY !== 'string') {
-	throw new Error('set NEXT_PUBLIC_GEMINI_API_KEY in .env.local');
+	throw new Error('set REACT_APP_GEMINI_API_KEY in .env');
 }
 
 const apiOptions: LiveClientOptions = {
