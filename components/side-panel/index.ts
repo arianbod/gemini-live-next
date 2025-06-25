bafee9bb44a26695658efe84289b8ae61b011ffb
Arian <130873720+arianbod@users.noreply.github.com>
2025-06-26 02:26:10 +0300

    v.1.2 uneccessatyy removed

--- components/side-panel/index.ts
+++
@@ -1,4 +0,0 @@
-// File location: components/side-panel/index.ts
-export { default } from './SidePanel';
-export { SidePanel } from './SidePanel';
-export type { LoggerFilterType } from './SidePanel';
